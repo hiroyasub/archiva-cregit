3f1bb0f21e3946b9202f6c176a7b8da7e470de85
Edwin L. Punzalan <epunzalan@apache.org>
2005-12-27 09:39:08 +0000

    Removed unnecessary imports and added some missing license texts
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-manager/trunk@359195 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: db3869de1751308f396283c98e79d69c4e562823

--- maven-repository-indexer/src/main/java/org/apache/maven/repository/indexing/ArtifactRepositoryIndexAnalyzer.java
+++ maven-repository-indexer/src/main/java/org/apache/maven/repository/indexing/ArtifactRepositoryIndexAnalyzer.java
@@ -29,26 +29,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|HashMap
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Map
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
