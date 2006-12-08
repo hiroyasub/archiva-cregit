a2be0b8945744908a36484141545ec0c2bb37f2c
Henri Yandell <bayard@apache.org>
2006-12-08 22:33:49 +0000

    Moved to using Commons IO. Just a package name change.
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@484829 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 24c1960dcbc85091b3ea43c7b4f645fd1e75d01d

--- archiva-indexer/src/test/java/org/apache/maven/archiva/indexer/lucene/LuceneMinimalArtifactIndexSearchTest.java
+++ archiva-indexer/src/test/java/org/apache/maven/archiva/indexer/lucene/LuceneMinimalArtifactIndexSearchTest.java
@@ -285,11 +285,11 @@ begin_import
 import|import
 name|org
 operator|.
-name|codehaus
+name|apache
 operator|.
-name|plexus
+name|commons
 operator|.
-name|util
+name|io
 operator|.
 name|FileUtils
 import|;
