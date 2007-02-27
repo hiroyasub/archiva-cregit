ee930e6480ab442fb4cf93d8565f587fe145c3b9
Joakim Erdfelt <joakime@apache.org>
2007-02-27 22:48:37 +0000

    Stab at model.
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@512468 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6d3a2dca4c19da2639bc07d659409d2ac5e2f473

---
+++ archiva-model/src/main/java/org/apache/maven/archiva/model/health/ArtifactHealth.java
@@ -0,0 +1,26 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|maven
+operator|.
+name|archiva
+operator|.
+name|model
+operator|.
+name|health
+package|;
+end_package
+
+begin_class
+specifier|public
+class|class
+name|ArtifactHealth
+block|{  }
+end_class
+
+end_unit
+
