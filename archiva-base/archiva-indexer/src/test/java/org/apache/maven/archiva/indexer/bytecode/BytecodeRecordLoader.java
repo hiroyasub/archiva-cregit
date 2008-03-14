fada2668c62d8e51ab99a2ad2933d0544560cc07
Joakim Erdfelt <joakime@apache.org>
2008-03-14 05:01:56 +0000

    [MRM-709] Use commons-io instead of Plexus Utils * Some more IOUtil and FileUtil conversions.
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@636993 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1e7c8f6da44eaba619664487d32abddf5cd3bb5e

--- archiva-base/archiva-indexer/src/test/java/org/apache/maven/archiva/indexer/bytecode/BytecodeRecordLoader.java
+++ archiva-base/archiva-indexer/src/test/java/org/apache/maven/archiva/indexer/bytecode/BytecodeRecordLoader.java
@@ -16,7 +16,7 @@ package|;
 end_package
 
 begin_comment
-comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *  http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *   http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
 begin_import
@@ -26,6 +26,20 @@ operator|.
 name|framework
 operator|.
 name|AssertionFailedError
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|commons
+operator|.
+name|io
+operator|.
+name|IOUtils
 import|;
 end_import
 
@@ -76,20 +90,6 @@ operator|.
 name|platform
 operator|.
 name|JavaArtifactHelper
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|codehaus
-operator|.
-name|plexus
-operator|.
-name|util
-operator|.
-name|IOUtil
 import|;
 end_import
 
@@ -627,16 +627,16 @@ throw|;
 block|}
 finally|finally
 block|{
-name|IOUtil
+name|IOUtils
 operator|.
-name|close
+name|closeQuietly
 argument_list|(
 name|reader
 argument_list|)
 expr_stmt|;
-name|IOUtil
+name|IOUtils
 operator|.
-name|close
+name|closeQuietly
 argument_list|(
 name|freader
 argument_list|)
