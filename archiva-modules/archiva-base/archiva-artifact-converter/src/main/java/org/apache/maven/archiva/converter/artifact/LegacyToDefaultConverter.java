853d1651d22d56a4e10ddaa1c4ec97316c85f4d2
Olivier Lamy <olamy@apache.org>
2011-09-03 08:27:41 +0000

    get RID of org.apache.maven.archiva packages : transaction package
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1164829 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 72a5da6643d4e352d1ab772b9d87cfe40b1dd401

--- archiva-modules/archiva-base/archiva-artifact-converter/src/main/java/org/apache/maven/archiva/converter/artifact/LegacyToDefaultConverter.java
+++ archiva-modules/archiva-base/archiva-artifact-converter/src/main/java/org/apache/maven/archiva/converter/artifact/LegacyToDefaultConverter.java
@@ -101,8 +101,6 @@ name|org
 operator|.
 name|apache
 operator|.
-name|maven
-operator|.
 name|archiva
 operator|.
 name|transaction
@@ -116,8 +114,6 @@ import|import
 name|org
 operator|.
 name|apache
-operator|.
-name|maven
 operator|.
 name|archiva
 operator|.
