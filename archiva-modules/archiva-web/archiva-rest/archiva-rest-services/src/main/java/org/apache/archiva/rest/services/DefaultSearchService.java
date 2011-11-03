40846a0ad547baead8b079ba8c33bd06691cafc2
Olivier Lamy <olamy@apache.org>
2011-11-03 15:19:00 +0000

    fix formatting
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1197180 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d3fe5410c76033a49085ed2eb0bb58b9db4cfd12

--- archiva-modules/archiva-web/archiva-rest/archiva-rest-services/src/main/java/org/apache/archiva/rest/services/DefaultSearchService.java
+++ archiva-modules/archiva-web/archiva-rest/archiva-rest-services/src/main/java/org/apache/archiva/rest/services/DefaultSearchService.java
@@ -475,16 +475,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Collection
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Collections
 import|;
 end_import
