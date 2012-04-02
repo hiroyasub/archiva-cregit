5d029267a52413daf43358ee4de4234a4fe29f85
Olivier Lamy <olamy@apache.org>
2012-04-02 21:02:29 +0000

    fix junit as now non absolute path is replace with appserver.base as parent
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1308562 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 741723f05a62a987d51b97e58b63ef8c71902124

--- archiva-modules/archiva-web/archiva-rest/archiva-rest-services/src/test/java/org/apache/archiva/rest/services/AbstractArchivaRestTest.java
+++ archiva-modules/archiva-web/archiva-rest/archiva-rest-services/src/test/java/org/apache/archiva/rest/services/AbstractArchivaRestTest.java
@@ -2294,6 +2294,20 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+specifier|public
+name|String
+name|getBasedir
+parameter_list|()
+block|{
+return|return
+name|System
+operator|.
+name|getProperty
+argument_list|(
+literal|"basedir"
+argument_list|)
+return|;
+block|}
 block|}
 end_class
 
