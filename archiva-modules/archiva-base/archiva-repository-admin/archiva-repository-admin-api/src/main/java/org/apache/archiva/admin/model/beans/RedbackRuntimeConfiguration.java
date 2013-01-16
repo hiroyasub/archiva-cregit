e25de370442ae76ec09f904e24f9ba10ff5c5425
Olivier Lamy <olamy@apache.org>
2013-01-16 21:48:35 +0000

    fix dynamic usage of rbac manager impl
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1434422 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6c05482be364438daadd4f8b6e3024aaf2f9769d

--- archiva-modules/archiva-base/archiva-repository-admin/archiva-repository-admin-api/src/main/java/org/apache/archiva/admin/model/beans/RedbackRuntimeConfiguration.java
+++ archiva-modules/archiva-base/archiva-repository-admin/archiva-repository-admin-api/src/main/java/org/apache/archiva/admin/model/beans/RedbackRuntimeConfiguration.java
@@ -127,6 +127,13 @@ argument_list|<
 name|String
 argument_list|>
 name|rbacManagerImpls
+init|=
+operator|new
+name|ArrayList
+argument_list|<
+name|String
+argument_list|>
+argument_list|()
 decl_stmt|;
 specifier|private
 name|LdapConfiguration
