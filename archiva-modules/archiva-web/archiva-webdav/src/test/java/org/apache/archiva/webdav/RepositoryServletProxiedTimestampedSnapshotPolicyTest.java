7ebb4814ea1ef7ba8de7a25ba390f8fd0ddb7c9c
Olivier Lamy <olamy@apache.org>
2014-01-06 05:31:33 +0000

    fix unit test
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1555671 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3bef8aec700a2f1bf6a572f2b68bc74762f2d191

--- archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/RepositoryServletProxiedTimestampedSnapshotPolicyTest.java
+++ archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/RepositoryServletProxiedTimestampedSnapshotPolicyTest.java
@@ -137,6 +137,11 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|super
+operator|.
+name|setUp
+argument_list|()
+expr_stmt|;
 name|archivaConfiguration
 operator|.
 name|getConfiguration
@@ -151,11 +156,6 @@ name|ProxyConnectorConfiguration
 argument_list|>
 argument_list|()
 argument_list|)
-expr_stmt|;
-name|super
-operator|.
-name|setUp
-argument_list|()
 expr_stmt|;
 block|}
 annotation|@
