5851d4041a01dbc492ac7015e929adf3fbb8606b
Martin Stockhammer <martin_s@apache.org>
2021-12-24 12:41:05 +0100

    Package refactoring
    Former-commit-id: a8e5b3a7b2c058c50ebe36514c5c74445cda8fe8

--- archiva-modules/archiva-maven/archiva-maven-indexer/src/test/java/org/apache/archiva/maven/indexer/search/MavenRepositorySearchTest.java
+++ archiva-modules/archiva-maven/archiva-maven-indexer/src/test/java/org/apache/archiva/maven/indexer/search/MavenRepositorySearchTest.java
@@ -7,16 +7,16 @@ name|apache
 operator|.
 name|archiva
 operator|.
-name|indexer
-operator|.
 name|maven
+operator|.
+name|indexer
 operator|.
 name|search
 package|;
 end_package
 
 begin_comment
-comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *  http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
 begin_import
@@ -123,6 +123,8 @@ name|apache
 operator|.
 name|archiva
 operator|.
+name|maven
+operator|.
 name|indexer
 operator|.
 name|util
@@ -144,24 +146,6 @@ operator|.
 name|utils
 operator|.
 name|ArchivaSpringJUnit4ClassRunner
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|maven
-operator|.
-name|index_shaded
-operator|.
-name|lucene
-operator|.
-name|index
-operator|.
-name|IndexUpgrader
 import|;
 end_import
 
@@ -208,18 +192,6 @@ operator|.
 name|context
 operator|.
 name|ContextConfiguration
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|nio
-operator|.
-name|file
-operator|.
-name|Files
 import|;
 end_import
 
